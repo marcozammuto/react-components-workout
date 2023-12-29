e463e7a38a6c3191cd8eefcb8c2719b339cd20b5
giuseppe zammuto <mrczammuto@gmail.com>
2023-12-29 23:37:16 +0100

    Js updated

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,9 @@
-import ToDoList from "./toDoList/ToDoList";
 import RandomQuoteGenerator from "./randomQuoteGenerator/RandomQuoteGenerator";
 import Keyboard from "./keyboard/Keyboard";
 import Mondo from "./locationSelector/LocationSelector";
 import PomodoroTimer from "./pomodoroTimer/PomodoroTimer";
-
+// lists of components
 export {
-  Calculator,
-  ToDoList,
   RandomQuoteGenerator,
   Keyboard,
   Mondo,
